ab8e78a6050117acedbde3315908df9e3282fe2b
el-waveconzults <elwaveservices@gmail.com>
2025-07-22 00:31:24 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+ HEAD
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+  theme: {
+      
+    extend: {
+        colors: {
+          'primary-orange': '#FF9F43',
+        },
+        maxWidth: {
+        'custom': '1440px',
+        },
+        screens: {
+          'xs': '320px', // Custom breakpoint for 320px and above
+        },
+      },
+    },
+    plugins: [],
+  };
+
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+  theme: {
+      
+    extend: {
+        colors: {
+          'primary-orange': '#FF9F43',
+        },
+        maxWidth: {
+        'custom': '1440px',
+        },
+        screens: {
+          'xs': '320px', // Custom breakpoint for 320px and above
+        },
+      },
+    },
+    plugins: [],
+  };
+ 40795cedd8f2d2f9ed04a13309d5ee525c6129ea
